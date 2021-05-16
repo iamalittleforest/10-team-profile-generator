0d7934d5c3260908d6a18f43406922bd8c5a36cd
Wendy Kobayashi <iamalittleforest@gmail.com>
2021-05-16 16:29:58 -0700

    draft engineer test

---
+++ _tests_/Engineer.test.js
@@ -0,0 +1,19 @@
+const Engineer = require('../lib/Engineer');
+
+describe('Engineer class', () => {
+  it('Create new Engineer object', () => {
+    
+  });
+
+  describe('getGithub method', () => {
+    it('Get Engineer GitHub', () => {
+
+    })
+  });
+
+  describe('getRole method', () => {
+    it('Get Engineer Role', () => {
+      
+    })
+  });
+});
