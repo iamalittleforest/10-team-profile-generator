f773cea8c685fc50c2fbca6538223cee05b00c36
Wendy Kobayashi <iamalittleforest@gmail.com>
2021-05-16 16:36:53 -0700

    draft manager test

---
+++ _tests_/Manager.test.js
@@ -0,0 +1,19 @@
+const Manager = require('../lib/Manager');
+
+describe('Manager class', () => {
+  it('Create new Manager object', () => {
+    
+  });
+
+  describe('getOfficeNumber method', () => {
+    it('Get Manager Office Number', () => {
+
+    })
+  });
+
+  describe('getRole method', () => {
+    it('Get Manager Role', () => {
+      
+    })
+  });
+});
