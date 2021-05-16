43b97fd415e1714d0de45fa8bd8d56d555feb0fe
Wendy Kobayashi <iamalittleforest@gmail.com>
2021-05-16 16:35:31 -0700

    draft intern test

---
+++ _tests_/Intern.test.js
@@ -0,0 +1,19 @@
+const Intern = require('../lib/Intern');
+
+describe('Intern class', () => {
+  it('Create new Intern object', () => {
+    
+  });
+
+  describe('getSchool method', () => {
+    it('Get Intern School', () => {
+
+    })
+  });
+
+  describe('getRole method', () => {
+    it('Get Intern Role', () => {
+      
+    })
+  });
+});
