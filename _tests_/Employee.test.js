2ed2977485d4c32b5e25e662c21c8f49afd8ad65
Wendy Kobayashi <iamalittleforest@gmail.com>
2021-05-16 16:24:29 -0700

    draft employee test

---
+++ _tests_/Employee.test.js
@@ -0,0 +1,31 @@
+const Employee = require('../lib/Employee');
+
+describe('Employee class', () => {
+  it('Create new Employee object', () => {
+    
+  });
+
+  describe('getName method', () => {
+    it('Get Employee Name', () => {
+
+    })
+  });
+
+  describe('getId method', () => {
+    it('Get Employee ID', () => {
+      
+    })
+  });
+
+  describe('getEmail method', () => {
+    it('Get Employee Email', () => {
+      
+    })
+  });
+
+  describe('getRole method', () => {
+    it('Get Employee Role', () => {
+      
+    })
+  });
+});
