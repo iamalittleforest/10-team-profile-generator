fca21460930d97d70f0830c4632b9c5c73c91440
Wendy Kobayashi <iamalittleforest@gmail.com>
2021-05-16 18:21:00 -0700

    added individual tests to manager test

--- _tests_/Manager.test.js
+++ _tests_/Manager.test.js
@@ -2,18 +2,24 @@ const Manager = require('../lib/Manager');
 
 describe('Manager class', () => {
   it('Create new Manager object', () => {
-    
+    const manager = new Manager('Rintaro Okabe', '001', 'okarin@futuregadgetlab.com', '5100');
+
+    expect(manager.officeNumber).toBe('5100');
   });
 
   describe('getOfficeNumber method', () => {
     it('Get Office Number of Manager', () => {
+      const manager = new Manager('Rintaro Okabe', '001', 'okarin@futuregadgetlab.com', '5100');
 
+      expect(manager.getOfficeNumber()).toBe('5100');
     })
   });
 
   describe('getRole method', () => {
     it('Get Role of Manager', () => {
-      
+      const manager = new Manager('Rintaro Okabe', '001', 'okarin@futuregadgetlab.com', '5100');
+
+      expect(manager.getRole()).toBe('Manager');
     })
   });
 });
