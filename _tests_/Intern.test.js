79399dd3b66822945b681457181a712c52e0384c
Wendy Kobayashi <iamalittleforest@gmail.com>
2021-05-16 18:16:47 -0700

    added individual tests to intern test

--- _tests_/Intern.test.js
+++ _tests_/Intern.test.js
@@ -2,18 +2,24 @@ const Intern = require('../lib/Intern');
 
 describe('Intern class', () => {
   it('Create new Intern object', () => {
-    
+    const intern = new Intern('Kurisu Makise', '004', 'christina@futuregadgetlab.com', 'Viktor Chondria University');
+
+    expect(intern.school).toBe('Viktor Chondria University');
   });
 
   describe('getSchool method', () => {
     it('Get School of Intern', () => {
+      const intern = new Intern('Kurisu Makise', '004', 'christina@futuregadgetlab.com', 'Viktor Chondria University');
 
+      expect(intern.getSchool()).toBe('Viktor Chondria University');
     })
   });
 
   describe('getRole method', () => {
     it('Get Role of Intern', () => {
-      
+      const intern = new Intern('Kurisu Makise', '004', 'christina@futuregadgetlab.com', 'Viktor Chondria University');
+
+      expect(intern.getRole()).toBe('Intern');
     })
   });
 });
