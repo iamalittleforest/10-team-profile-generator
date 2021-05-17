a010b1418b47d9249a48db43c7fda020ed19b6f3
Wendy Kobayashi <iamalittleforest@gmail.com>
2021-05-16 22:12:45 -0700

    initial draft of index.js

---
+++ index.js
@@ -0,0 +1,15 @@
+// import external packages
+const fs = require('fs'); 
+const inquirer = require('inquirer');
+
+// import team roles
+const Engineer = require('./lib/Engineer');
+const Intern = require('./lib/Intern'); 
+const Manager = require('./lib/Manager');
+
+// team
+const team = [];
+
+// function to obtain input
+
+// function to write data to file
