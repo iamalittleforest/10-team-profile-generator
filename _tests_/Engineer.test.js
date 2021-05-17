ff75359bbff6267dcff171f71c860ed3fab61c5a
Wendy Kobayashi <iamalittleforest@gmail.com>
2021-05-16 18:09:31 -0700

    added individual tests to engineer test

--- _tests_/Engineer.test.js
+++ _tests_/Engineer.test.js
@@ -2,18 +2,24 @@ const Engineer = require('../lib/Engineer');
 
 describe('Engineer class', () => {
   it('Create new Engineer object', () => {
-    
+    const engineer = new Engineer('Itaru Hashida', '003', 'daru@futuregadgetlab.com', 'daru');
+
+    expect(engineer.github).toBe('daru');
   });
 
   describe('getGithub method', () => {
     it('Get GitHub of Engineer', () => {
+      const engineer = new Engineer('Itaru Hashida', '003', 'daru@futuregadgetlab.com', 'daru');
 
+      expect(engineer.getGithub()).toBe('daru');
     })
   });
 
   describe('getRole method', () => {
     it('Get Role of Engineer', () => {
-      
+      const engineer = new Engineer('Itaru Hashida', '003', 'daru@futuregadgetlab.com', 'daru');
+
+      expect(engineer.getRole()).toBe('Engineer');
     })
   });
 });
