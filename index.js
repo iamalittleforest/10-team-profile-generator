ca3462a241b71b153f800df7adeae18099675cde
Wendy Kobayashi <iamalittleforest@gmail.com>
2021-05-17 21:04:11 -0700

    added engineer portion of addEmployee function

--- index.js
+++ index.js
@@ -48,12 +48,69 @@ const manager = () => {
     })
 }
 
+// function to obtain employee info
+const addEmployee = () => {
+  return inquirer
+    .prompt([
+      {
+        type: 'list',
+        name: 'addEmployee',
+        message: "What type of employee would you like to add?",
+        choices: ['Engineer', 'Intern', 'None']
+      }
+    ])
+    .then((response) => {
+      if(response.addEmployee === 'Engineer') {
+        return inquirer
+          .prompt([
+            {
+              type: 'input',
+              name: 'name',
+              message: "What is the engineer's name?"
+              // add validate
+            },
+            {
+              type: 'input',
+              name: 'id',
+              message: "What is the engineer's ID number?"
+              // add validate
+            },
+            {
+              type: 'input',
+              name: 'email',
+              message: "What is the engineer's email address?"
+              // add validate
+            },
+            {
+              type: 'input',
+              name: 'officeNumber',
+              message: "What is the engineer's office number?"
+              // add validate
+            },
+            {
+              type: 'input',
+              name: 'github',
+              message: "What is the gngineer's GitHub username?"
+            }
+          ])
+          .then((engineer) => {
+            // create engineer object using responses
+            const engineer = new Engineer(engineer.name, engineer.id, engineer.email, engineer.github);
+
+            // add manager object to team array
+            team.push(engineer);
+         })
+      }
+    })
+}
+
 // function to write data to file
 
 // function to intialize data input
 const init = () => {
   console.log("Please build your team");
   manager();
+  addEmployee();
 };
 
 // initialize
