17a76346e6665e041816d26269275c8b0d736cc1
Wendy Kobayashi <iamalittleforest@gmail.com>
2021-05-18 12:10:10 -0700

    created separate function for different roles

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const Manager = require('./lib/Manager');
 const team = [];
 
 // function to obtain manager info
-const manager = () => {
+const managerPrompt = () => {
   return inquirer
     .prompt([
       {
@@ -37,22 +37,7 @@ const manager = () => {
         name: 'officeNumber',
         message: "What is the team manager's office number?"
         // add validate
-      }
-    ])
-    .then((response) => {
-      // create manager object using responses
-      const manager = new Manager(response.name, response.id, response.email, response.officeNumber);
-
-      // add manager object to team array
-      team.push(manager);
-      console.log(team);
-    })
-}
-
-// function to obtain employee info
-const addEmployee = () => {
-  return inquirer
-    .prompt([
+      },
       {
         type: 'list',
         name: 'addEmployee',
@@ -60,92 +45,121 @@ const addEmployee = () => {
         choices: ['Engineer', 'Intern', 'None']
       }
     ])
-    .then((response) => {
+    .then((mResponse) => {
+      // create manager object using responses
+      const manager = new Manager(mResponse.name, mResponse.id, mResponse.email, mResponse.officeNumber);
+
+      // add manager object to team array
+      team.push(manager);
+      
       // obtain info specific to the selected role
-      if(response.addEmployee === 'Engineer') {
-        return inquirer
-          .prompt([
-            {
-              type: 'input',
-              name: 'name',
-              message: "What is the engineer's name?"
-              // add validate
-            },
-            {
-              type: 'input',
-              name: 'id',
-              message: "What is the engineer's ID number?"
-              // add validate
-            },
-            {
-              type: 'input',
-              name: 'email',
-              message: "What is the engineer's email address?"
-              // add validate
-            },
-            {
-              type: 'input',
-              name: 'github',
-              message: "What is the gngineer's GitHub username?"
-            }
-          ])
-          .then((response) => {
-            // create engineer object using responses
-            const engineer = new Engineer(response.name, response.id, response.email, response.github);
-
-            // add engineer object to team array
-            team.push(engineer);
-            console.log(team);
-         })
-      } else if (response.addEmployee === 'Intern') {
-        return inquirer
-          .prompt([
-            {
-              type: 'input',
-              name: 'name',
-              message: "What is the intern's name?"
-              // add validate
-            },
-            {
-              type: 'input',
-              name: 'id',
-              message: "What is the intern's ID number?"
-              // add validate
-            },
-            {
-              type: 'input',
-              name: 'email',
-              message: "What is the intern's email address?"
-              // add validate
-            },
-            {
-              type: 'input',
-              name: 'school',
-              message: "What is the intern's school name?"
-            }
-          ])
-          .then((response) => {
-            // create intern object using responses
-            const intern = new Intern(response.name, response.id, response.email, response.school);
-
-            // add intern object to team array
-            team.push(intern);
-            console.log(team);
-        })
+      if(mResponse.addEmployee === 'Engineer') {
+        engineerPrompt();
+      } else if (mResponse.addEmployee === 'Intern') {
+        internPrompt();
       } else {
-        // how to end inquirer
+        generateHTML();
       }
     })
-}
+  }
+  
+  // function to obtain engineer info
+  const engineerPrompt = () => {
+    return inquirer
+    .prompt([
+      {
+        type: 'input',
+        name: 'name',
+        message: "What is the engineer's name?"
+        // add validate
+      },
+      {
+        type: 'input',
+        name: 'id',
+        message: "What is the engineer's ID number?"
+        // add validate
+      },
+      {
+        type: 'input',
+        name: 'email',
+        message: "What is the engineer's email address?"
+        // add validate
+      },
+      {
+        type: 'input',
+        name: 'github',
+        message: "What is the engineer's GitHub username?"
+        // add validate
+      },
+      {
+        type: 'list',
+        name: 'addEmployee',
+        message: "What type of employee would you like to add?",
+        choices: ['Engineer', 'Intern', 'None']
+      }
+    ])
+  .then((eResponse) => {
+    // create engineer object using responses
+    const engineer = new Engineer(eResponse.name, eResponse.id, eResponse.email, eResponse.github);
+    
+    // add engineer object to team array
+    team.push(engineer);
+  })
+}     
+  
+// function to obtain intern info
+const internPrompt = () => {
+  return inquirer
+    .prompt([
+      {
+          type: 'input',
+          name: 'name',
+          message: "What is the intern's name?"
+          // add validate
+        },
+        {
+          type: 'input',
+          name: 'id',
+          message: "What is the intern's ID number?"
+          // add validate
+        },
+        {
+          type: 'input',
+          name: 'email',
+          message: "What is the intern's email address?"
+          // add validate
+        },
+        {
+          type: 'input',
+          name: 'school',
+          message: "What is the intern's school name?"
+          // add validate
+        },
+        {
+          type: 'list',
+          name: 'addEmployee',
+          message: "What type of employee would you like to add?",
+          choices: ['Engineer', 'Intern', 'None']
+        }
+      ])
+    .then((iResponse) => {
+      // create intern object using responses
+      const intern = new Intern(iResponse.name, iResponse.id, iResponse.email, iResponse.school);
+      
+      // add intern object to team array
+      team.push(intern);
+    })
+  }
+  
+  // function to write data to file
+  const generateHTML = () => {
 
-// function to write data to file
-
-
-// function to intialize data input
-const init = () => {
-  console.log("Please build your team");
-  manager();
-  addEmployee();
+  }
+  
+  // function to intialize data input
+  const init = () => {
+    console.log("Please build your team");
+    managerPrompt();
 };
 
 // initialize
